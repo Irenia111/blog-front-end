07a5c14ecf376ff92ba1b77820d79a3d0ba71534
Irenia111 <15000616693@163.com>
2021-04-09 11:27:05 +0800

    edit blog page

--- static/js/admin.js
+++ static/js/admin.js
@@ -1,3 +1,17 @@
 $('.ui.dropdown')
   .dropdown()
 ;
+
+// 表单验证
+$('.ui.form').form({
+  fields: {
+    title: {
+      identifier: 'title',
+      rules: [{
+        type: 'empty',
+        prompt: '标题：标题未输入'
+      }]
+    }
+  }
+})
+;
