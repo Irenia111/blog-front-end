982ae7e70d203b35170387d9d0471fa3b3181b29
Irenia111 <15000616693@163.com>
2021-04-14 11:39:51 +0800

    modify the nav

--- static/js/index.js
+++ static/js/index.js
@@ -1,4 +1,12 @@
 //$('.ui.sticky').sticky();
-$('.menu-toggle').click(function (){
+$('.menu-toggle').click(() => {
   $('.nav-item').toggleClass('m-mobile-hide');
 });
+$('.ui.sidebar').sidebar('attach events', '#toc');
+/*$(".toc.item").click(function () {
+  $(".ui.sidebar")
+    .sidebar('setting', 'transition', 'uncover')
+    .sidebar("toggle");
+});
+
+ */
