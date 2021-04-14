1b7745b6ea9c4b7d91da5f92e0db7632047ef197
Irenia111 <15000616693@163.com>
2021-04-14 13:52:36 +0800

    modify the toc

--- static/js/index.js
+++ static/js/index.js
@@ -1,12 +1,3 @@
-//$('.ui.sticky').sticky();
-$('.menu-toggle').click(() => {
-  $('.nav-item').toggleClass('m-mobile-hide');
-});
-$('.ui.sidebar').sidebar('attach events', '#toc');
-/*$(".toc.item").click(function () {
-  $(".ui.sidebar")
-    .sidebar('setting', 'transition', 'uncover')
-    .sidebar("toggle");
-});
+$('.ui.sticky').sticky();
 
- */
+$('.ui.sidebar').sidebar('attach events', '#toc');
